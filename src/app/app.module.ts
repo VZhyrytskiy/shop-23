966188d749cd9ebef79366f8b2fc00e512735ebb
Pylyp Cherpakov <Pylyp_Cherpakov@epam.com>
2020-05-06 09:05:19 +0300

    [feature/task1_5-6]: ProductsList and Product comp.
    1. Created general structure for features;
    2. Implemented ProductsList and Product components;
    3. Implemented ProductsService;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './products/components/product/product.component';
+import { ProductsListComponent } from './products/components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
