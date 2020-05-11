5e6e228990a222539b83bb892da454c636d424c5
ksavitskiy <kirillsavik@gmail.com>
2020-05-11 22:05:25 +0300

    [feature/task2_4-5]: Add remove/clear/getInfo methods to cart.service

--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -1,21 +1,46 @@
 import { Injectable } from '@angular/core';
 
 import { Product } from 'src/app/products/models/product.model';
+import { Cart } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class CartService{
-  private purchasedProducts: Array<Product> = [];
+  private currentCart: Cart = new Cart();
 
-  constructor() {}
-
-  getPurchasedProducts() {
-    return this.purchasedProducts;
+  constructor() {
+    this.currentCart.purchasedProducts = [];
+    this.currentCart.totalPrice = 0;
+    this.currentCart.totalProducts = 0;
   }
 
-  addToCart(product: Product) {
-    this.purchasedProducts.push(product);
+  getPurchasedProducts() {
+    return this.currentCart.purchasedProducts;
+  }
+
+  getCartInfo(){
+    return this.currentCart;
+  }
+
+  addProductToCart(product: Product) {
+    this.currentCart.purchasedProducts.push(product);
+    this.currentCart.totalPrice += product.price;
+    this.currentCart.totalProducts = this.currentCart.purchasedProducts.length;
+  }
+
+  remoreProductFromCart(product: Product){
+    this.currentCart.purchasedProducts.forEach((item, index) => {
+      if (item === product) { this.currentCart.purchasedProducts.splice(index, 1); }
+    });
+    this.currentCart.totalPrice -= product.price;
+    this.currentCart.totalProducts = this.currentCart.purchasedProducts.length;
+  }
+
+  clearCart(){
+    this.currentCart.purchasedProducts.splice(0, this.currentCart.purchasedProducts.length);
+    this.currentCart.totalPrice = 0;
+    this.currentCart.totalProducts = 0;
   }
 }
