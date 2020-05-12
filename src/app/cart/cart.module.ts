d64b426507f150647613c1339a25f796871db418
ksavitskiy <kirillsavik@gmail.com>
2020-05-12 21:26:50 +0300

    [feature/task2_4-5]: Rename cart.component to cart-list.component and create cart-item.component

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CartListComponent } from './components/cart-list.component';
+
+import { CartListComponent } from './components/cart-list/cart-list.component';
 import { CartService } from './services/cart.service';
-import { CartItemComponent } from './components/cart-item.component';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
 
 @NgModule({
   imports: [CommonModule],
