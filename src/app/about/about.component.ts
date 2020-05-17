491f8dd9c3f47b72de2eaaea7c32ea426cbcccd2
Pylyp Cherpakov <Pylyp_Cherpakov@epam.com>
2020-05-17 13:58:06 +0300

    [feature/task3_4-5-7]: Implemented ConstantsService

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Optional } from '@angular/core';
+import { ConstantServiceToken, ConstantService } from '../core';
+
+const appInfo = { App: "Devices Shop", Ver: "1.0" };
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  providers: [{ provide: ConstantServiceToken, useValue: appInfo }]
 })
 export class AboutComponent implements OnInit {
+  appName: string;
+  appVersion: string;
 
-  constructor() { }
+  constructor(
+    @Inject(ConstantServiceToken)@Optional() public appInfo: ConstantService
+  ) { }
 
   ngOnInit(): void {
+    this.appName = appInfo.App;
+    this.appVersion = appInfo.Ver;
   }
-
 }
---
+++ src/app/core/services/constants.service.ts
@@ -0,0 +1,8 @@
+import { InjectionToken } from '@angular/core';
+
+export interface ConstantService {
+  App: string,
+  Ver: string
+}
+
+export const ConstantServiceToken = new InjectionToken<ConstantService>('ConstantService Token');
