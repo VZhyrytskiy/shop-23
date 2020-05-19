779011f080b6b1a9c99f1991d1acab3c84a8fd35
Pylyp Cherpakov <Pylyp_Cherpakov@epam.com>
2020-05-19 20:23:53 +0300

    [feature/task4_3-4]: Implemented orderBy pipe (not working)

---
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'orderBy'
+})
+export class OrderByPipe implements PipeTransform {
+
+  transform(arr: Array<any>, field: string, order: 'up' | 'down' = 'down') {
+    return arr.sort((a, b) => {
+      const value1 = a[field];
+      const value2 = b[field];
+
+      if (value1 > value2) {
+        return order === 'up' ? 1 : -1;
+      }
+      else if (value1 < value2) {
+        return order === 'up' ? -1 : 1;
+      }
+
+      return 0;
+    });
+  }
+  
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChangeBackgroundDirective, RandomColorDirective } from './directives';
+import { OrderByPipe } from './pipes';
 
 const directives = [ChangeBackgroundDirective, RandomColorDirective];
+const pipes = [OrderByPipe];
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [...directives],
-  exports: [...directives]
+  declarations: [...directives, ...pipes],
+  exports: [...directives, ...pipes]
 })
 export class SharedModule {}
