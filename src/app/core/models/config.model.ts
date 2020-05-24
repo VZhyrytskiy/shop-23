cf1b3328a5a12496ab6962d13c7c453f03ffbd3a
ksavitskiy <kirillsavik@gmail.com>
2020-05-24 22:15:07 +0300

    [feature/task3_1-3]: Add ConfigOptionsService

---
+++ src/app/core/models/config.model.ts
@@ -0,0 +1,5 @@
+export interface Configuration{
+  id: number,
+  login: string,
+  email: string
+}
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,3 +2,4 @@ export * from './constants.service';
 export * from './generator.service';
 export * from './random-string-n.factory';
 export * from './local-storage.service';
+export * from './config-options.service';
