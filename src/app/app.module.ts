686cc4f2bb63e542527d1c82d7f464a6759b0a0a
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-05-26 14:55:02 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProductsModule } from './products/products.module';
+// import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -16,7 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    ProductsModule,
+    // ProductsModule,
     CartModule,
     AppRoutingModule
   ],
